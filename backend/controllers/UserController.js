732887a08e9beba704cf6eca5aba852d6ca8633f
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-24 20:18:30 -0300

    Update UserController.js

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
 
 const bcrypt = require('bcrypt')
+const createUserToken = require('../helpers/create-user-token')
 
 module.exports = class UserController {
   static async register ( req, res) {
@@ -56,7 +57,7 @@ module.exports = class UserController {
 
     try{
       const newUser = await user.save()
-      res.status(201).json({message: 'Usuário criado!', newUser})
+      await createUserToken(newUser, req, res)
     } catch(error) {
       res.status(500).json({message:error})
     }
