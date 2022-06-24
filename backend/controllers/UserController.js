77e9456cdf3f804f62478eb6e85bf6484f795041
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-24 10:41:17 -0300

    Update UserController.js

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -4,5 +4,24 @@ module.exports = class UserController {
   static async register ( req, res) {
     
     const {name , email , phone , password , confirmpassword} = req.body
+
+    //validations
+    if(!name){
+      res.status(422).json({message: 'O nome é obrigatório! '})
+      return
+    }
+    if(!email){
+      res.status(422).json({message: 'O e-mail é obrigatório! '})
+      return
+    }
+    if(!password){
+      res.status(422).json({message: 'O nome é obrigatório! '})
+      return
+    }
+    if(!confirmpassword){
+      res.status(422).json({message: ' nome é obrigatório! '})
+      return
+    }
+
   }
 }
