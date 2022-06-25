dabd30dcedf6ea0242e16b4e3ee474267d4a6d87
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-24 21:53:34 -0300

    Update verify-token.js

--- backend/helpers/verify-token.js
+++ backend/helpers/verify-token.js
@@ -1,10 +1,14 @@
 const jwt = require('jsonwebtoken')
+const getToken = require('./get-token')
 
 //middleware to validate token 
 const checkToken = (req, res , next) => {
 
-  
+  const token = getToken(req)
 
+  if(!token) {
+    return res.status(401).json({ message: 'Acesso Negado!'})
+  }
 }
 
 module.exports = checkToken
