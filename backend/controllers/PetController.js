d9152de091069f53e283015a79b14330833a9b0a
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-25 10:12:09 -0300

    Update PetController.js

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -4,7 +4,31 @@ module.exports = class PetController {
 
   //create a pet
   static async create (req , res) {
-    res.json( {message: 'Deu certo!'})
+    
+    const {name , age , weight , color } = req.body
+
+    const available = true
+
+    // image upload
+
+    // validations
+    if(!name) {
+      res.status(422).json({message: 'O nome é obrigatório!'})
+      return
+    }
+    if(!age) {
+      res.status(422).json({message: 'A idade é obrigatório!'})
+      return
+    }
+    if(!weight) {
+      res.status(422).json({message: 'O peso é obrigatório!'})
+      return
+    }
+    if(!color) {
+      res.status(422).json({message: 'A cor é obrigatória!'})
+      return
+    }
+
   }
 
 }
