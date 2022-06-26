eef0549f70a999656e3249be829ba575167b59a1
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-25 22:47:07 -0300

    Update PetController.js

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -109,7 +109,7 @@ module.exports = class PetController {
 
   static async getPetById( req, res) {
     const id = req.params.id
-
+    //check id is valid
     if(!ObjectId.isValid(id)) {
       res.status(422).json({message: 'ID inválido!'})
       return
@@ -125,5 +125,31 @@ module.exports = class PetController {
     res.status(200).json({
       pet: pet,
     })
+  }
+
+  static async removePetById(req, res) {
+
+    const id = req.params.id
+    //check id is valid
+    if(!ObjectId.isValid(id)) {
+      res.status(422).json({message: 'ID inválido!'})
+      return
+    }
+     // check if pet exists
+    const pet = await Pet.findOne({ _id: id })
+
+    if(!pet) {
+      res.status(404).json({ message: 'Pet não encontrado!'})
+    }
+
+    // check is logged in user registered the pet
+    //get user from token
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+
+    if(pet.user._id.toString() !== user._id){
+      res.status(422).json({ message: 'Houve um problema em processar a sua solicitação, tente novamente mais tarde!'})
+    }
+
   }
 }
