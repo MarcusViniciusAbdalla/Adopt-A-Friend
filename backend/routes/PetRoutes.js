129bd194ee201e3d785fea14c1619b670ac36a1b
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-25 22:31:03 -0300

    Update PetRoutes.js

--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -11,5 +11,6 @@ router.post('/create' , verifyToken, imageUpload.array('images'), PetController.
 router.get('/', PetController.getAll)
 router.get('/mypets' , verifyToken, PetController.getAllUserPets)
 router.get('myadoptions' , verifyToken, PetController.getAllUserAdoptions)
+router.get('/:id', PetController.getPetById)
 
 module.exports = router
