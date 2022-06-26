f5f3975474f5e3f1cb62bd6a79f178314a8b1ffe
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-26 18:55:15 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import {BrowserRouter as Router, Switch , Route} from 'react-router-dom'
 
+/* pages*/
+import Login from './components/pages/Auth/Login'
+import Register from './components/pages/Auth/Register'
+import Home from './components/pages/Auth/Home'
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/login">
-          
+          <Login/>
         </Route>
         <Route path="/register">
-          
+          <Register/>
         </Route>
         <Route path="/">
-          
+          <Home/>
         </Route>
       </Switch>
     </Router>
