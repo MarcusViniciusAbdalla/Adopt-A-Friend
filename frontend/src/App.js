40b13011676468cfb770ff9be3917062dbe1f4a4
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-26 18:28:56 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
+import {BrowserRouter as Router, Switch , Route} from 'react-router-dom'
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Adopt A Friend</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          
+        </Route>
+        <Route path="/register">
+          
+        </Route>
+        <Route path="/">
+          
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
