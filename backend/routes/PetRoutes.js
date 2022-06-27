a4484633130e1cdd0e877914f50e3718b5be3771
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-27 16:32:55 -0300

    Update PetRoutes.js

--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 const PetController = require("../controllers/PetController")
 
 // middlewares
-const verifyToken = require("../helpers/verify-token")
+const verifyToken = require("../helpers/check-token")
 const { imageUpload } = require('../helpers/image-upload')
 
 router.post('/create' , verifyToken, imageUpload.array('images'), PetController.create)
