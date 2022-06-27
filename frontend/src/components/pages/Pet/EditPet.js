1e09e2195be3c9aac2a877f5f3b267da2d45c1fb
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-27 01:48:22 -0300

    Update EditPet.js

--- frontend/src/components/pages/Pet/EditPet.js
+++ frontend/src/components/pages/Pet/EditPet.js
@@ -3,7 +3,7 @@ import api from '../../../utils/api'
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
-import styles from './AddPet.module.css'
+import styles from './Add.Pet.module.css'
 
 import PetForm from '../../form/PetForm'
 
