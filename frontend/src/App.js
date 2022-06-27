d4f52970aa2802cfeed9a34e65d29a7339375ef2
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-26 23:45:25 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,10 +12,11 @@ import Login from './components/pages/Auth/Login'
 import Register from './components/pages/Auth/Register'
 import Home from './components/pages/Home'
 import Profile from './components/pages/User/Profile'
-
+import MyPets from './components/pages/Pet/MyPets'
 
 /* context */
 import {UserProvider} from './context/UserContext'
+import MyPets from './components/pages/Pet/MyPets'
 
 function App() {
   return (
@@ -33,6 +34,9 @@ function App() {
             </Route>
             <Route path="/user/profile">
               <Profile />
+            </Route>
+            <Route path="/pet/mypets">
+              <MyPets />
             </Route>
             <Route path="/">
               <Home />
