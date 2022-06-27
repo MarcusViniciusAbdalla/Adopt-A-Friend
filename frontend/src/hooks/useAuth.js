b9dbad1f7078f294789891110bf95a785accc882
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-26 22:24:07 -0300

    Update useAuth.js

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -5,8 +5,18 @@ import {useHistory} from 'react-router-dom'
 import {useFlashMessage} from './useFlashMessage'
 
 export default function useAuth() {
-
+  const [authenticated, setAuthenticated] = useState(false) 
   const {setFlashMessage} = useFlashMessage()
+  const history = useHistory()
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if(token) {
+      api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`
+      setAuthenticated(true)
+    }
+
+  }, [])
 
   async function register(user) {
 
@@ -18,15 +28,20 @@ export default function useAuth() {
         return response.data
       })
 
-      console.log(data)
+      await authUser(data)
     } catch (error) {
-      // tratar erro
       msgText = error.response.data.message
       msgType = 'error'
     }
 
     setFlashMessage(msgText , msgType)
 
+    async function authUser(data) {
+      setAuthenticated(true)
+      localStorage.setItem('token' , JSON.stringify(data.token))
+      history.push('/')
+    }
+
   }
-  return { register }
+  return { authenticated ,register }
 }
