074b8951810f1fd342d7ccb53177913b02c91e97
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-27 16:33:00 -0300

    Update UserRoutes.js

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,9 +1,9 @@
-const router = require('express').Router()
+const router = require("express").Router()
 
 const UserController = require('../controllers/UserController')
 
 //middleware
-const verifyToken = require('../helpers/verify-token')
+const verifyToken = require('../helpers/check-token')
 const { imageUpload } = require("../helpers/image-upload")
 
 router.post('/register' , UserController.register)
